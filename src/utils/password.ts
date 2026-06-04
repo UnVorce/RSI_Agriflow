fcfe38b5dc4ada4e5998965ef58f58a3f340504f
UnVorce <xgamemastah@gmail.com>
2026-06-05 06:59:26 +0700

    Test2

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 
-const SALT_ROUNDS = 10;
+// TSD 6.2: Bcrypt cost 12 as per security specification
+const SALT_ROUNDS = 12;
 
 export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, SALT_ROUNDS);
